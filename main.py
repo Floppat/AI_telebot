d9783a042d65f8b87154414d174b13674d09eebe
who? <164739638+Floppat@users.noreply.github.com>
2025-06-03 22:44:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,51 @@
 import telebot
-import config
-import ya
-import db
 
-API_TOKEN = config.API_TOKEN
+from config import API_TOKEN, path
+from ya import Ai
+from db import DB_Manager
+
 
 bot = telebot.TeleBot(API_TOKEN)
-
-dab = db.DB_Manager(config.path)
-
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    print(type(message))
-    try:
-        dab.read(message.chat.id)
-    except IndexError:
-        if  message.from_user.id == message.chat.id:
-            dab.new_user(message.chat.id) 
-        else:
-            dab.new_chat(message.chat.id)
-    bot.reply_to(message, "Привет, чем я могу помочь?")
+dab = DB_Manager(path)
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message): # message: telebot.types.Message
+def handle_AI(message, additional):
+    bot.send_chat_action(message.chat.id,'typing')
+    ai = Ai(dab.read(message.chat.id)) # загрузка памяти
+    ai.new_prompt(additional + message.text) # вписываю запрос
+    bot.reply_to(message, asis := ai.gpt()) # генерирую ответ
+    ai.asis_ans(asis) # сохраняю ответ
+    dab.update(message.chat.id, ai.get_history()) # обновляю бд
+
+def check_reg(message):
     try:
         dab.read(message.chat.id)
     except IndexError:
         bot.reply_to(message, 'используй /start чтобы начать')
         return
+    
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    try:
+        dab.read(message.chat.id)
+    except IndexError:
+        if  message.from_user.id == message.chat.id:
+            dab.new_id(message.chat.id, 'user') 
+        else:
+            dab.new_id(message.chat.id, 'chat')
+    bot.reply_to(message, "Привет, чем я могу помочь?")
+    
+@bot.message_handler(func=lambda message: True)
+def echo_message(message): # message: telebot.types.Message
     if message.from_user.id == message.chat.id: #проверка - групповой чат или лс
-        bot.send_chat_action(message.chat.id,'typing')
-        ai = ya.Ai(dab.read(message.chat.id)['messages']) # загрузка памяти
-        ai.new_prompt(message.text) # вписываю запрос
-        bot.reply_to(message, asis := ai.gpt()) # генерирую ответ
-        ai.asis_ans(asis) # сохраняю ответ
-        dab.update(message.chat.id, ai.get_prompt()) # обновляю бд
+        check_reg(message)
+        handle_AI(message, '')
     elif (message.text is not None and ("@" + str(bot.get_me().username)) in message.text) or (message.reply_to_message is not None and message.reply_to_message.from_user.id == bot.get_me().id): # нечто сложное что написал не я
-        bot.send_chat_action(message.chat.id,'typing')
-        ai = ya.Ai(dab.read(message.chat.id)['messages']) # загрузка памяти
-        ai.new_prompt(f'{message.from_user.full_name} написал: {message.text}') # вписываю запрос
-        bot.reply_to(message, asis := ai.gpt()) # генерирую ответ
-        ai.asis_ans(asis) # сохраняю ответ
-        dab.update(message.chat.id, ai.get_prompt()) # обновляю бд
+        check_reg(message)
+        handle_AI(message, f'{message.from_user.full_name} написал: ')
     else:
         pass
+
 
 bot.infinity_polling()
